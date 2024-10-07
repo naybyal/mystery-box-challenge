f49572c006fb67026e0c8b02244d676019689b9f
naybyal <officiallynabiel@gmail.com>
2024-10-07 22:34:16 +0530

    refactor

--- app/Http/Controllers/BoxController.php
+++ app/Http/Controllers/BoxController.php
@@ -5,25 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class BoxController extends Controller {
-    // Display the list of mystery boxes
     public function showBoxes() {
         return view('boxes');
     }
-
-    // Reveal the prize inside the selected box
     public function revealPrize($boxId) {
         $prizes = [
-            '🎉 A Free Coffee!', 
-            '🍫 A Chocolate Bar!', 
-            '🚗 A Toy Car!', 
-            '🔑 A Key to Nothing!', 
+            '🎮 A Steamdeck!', 
+            '🍫 A Dark Chocolate Bar!', 
+            '🚗 A Porsche 911 GT3 RS', 
+            '🤡 ഒലക്ക!', 
             '🌟 A Star!'
         ];
 
-        // Randomly select a prize
         $prize = $prizes[array_rand($prizes)];
 
-        // Return the reveal view with the prize and boxId
         return view('reveal', ['prize' => $prize, 'boxId' => $boxId]);
     }
 }
