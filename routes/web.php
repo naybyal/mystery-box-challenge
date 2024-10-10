72c4c0eb5a42209ddad1fc98510c378c771bd65d
naybyal <officiallynabiel@gmail.com>
2024-10-10 21:33:50 +0530

    refactor

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ use App\Http\Controllers\GreetingController;
 
 Route::get('/', [BoxController::class, 'showBoxes'])->name('home');
 Route::get('/open-box/{boxId}', [BoxController::class, 'revealPrize'])->name('open-box');
-// Route::get('/greet', [GreetingController::class, 'displayGreeting'])->name('greet');
+Route::get('/greet', [GreetingController::class, 'displayGreeting'])->name('greet');
